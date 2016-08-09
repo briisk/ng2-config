0a14f9e104fe4c53d438ceb0976c9afe070066e8
Daniel Koza <dakolech@gmail.com>
2016-08-09 15:20:10 +0200

    update linters

--- pug.js
+++ pug.js
@@ -1,8 +1,9 @@
 #! /usr/bin/env node
 var shell = require('shelljs');
 var glob = require('glob');
+var path = require('path');
 
-glob('src/**/*.jade', function(er, files) {
-  shell.exit(shell.exec('pug-lint ' + files.join(' ')).code);
+glob('src/**/*.jade', { root: path.resolve(__dirname, '..') }, function(er, files) {
+  shell.exit(shell.exec('pug-lint ' + '-c ng2-config/.pug-lint.json ' + files.join(' ')).code);
 });
 
