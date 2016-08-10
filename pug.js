bd017311c987fbe150feef60b0704d86e3d252df
Daniel Koza <dakolech@gmail.com>
2016-08-10 14:26:18 +0200

    update path

--- pug.js
+++ pug.js
@@ -1,9 +1,8 @@
 #! /usr/bin/env node
 var shell = require('shelljs');
 var glob = require('glob');
-var path = require('path');
 
-glob('src/**/*.jade', { root: path.resolve(__dirname, '..') }, function(er, files) {
-  shell.exit(shell.exec('pug-lint ' + '-c ng2-config/.pug-lint.json ' + files.join(' ')).code);
+glob('src/**/*.jade', function(er, files) {
+  shell.exit(shell.exec('pug-lint ' + files.join(' ')).code);
 });
 
